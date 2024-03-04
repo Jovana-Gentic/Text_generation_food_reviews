1b0e18c937b396e273321ca1319716358b48632f
Jovana Gentić <141590441+Jovana-Gentic@users.noreply.github.com>
2024-03-04 19:05:10 +0100

    Update generate.py

--- generate.py
+++ generate.py
@@ -5,7 +5,15 @@ from nn.model import LSTMModel
 from utils.utils import create_checkpoint_manager, load_checkpoint_if_exists
 from utils.generate_helpers import generate_review
 
-model = LSTMModel()
+if 'lstm' in MODEL_NAME:
+    model = LSTMModel()
+elif 'cnn' in MODEL_NAME:
+    model = CNNModel()
+elif 'transformer' in MODEL_NAME:
+    model = transformer_model()
+else:
+    raise NotImplementedError(f'uuuuuh model {MODEL_NAME} type unknown!!!')
+  
 lr_schedule = ExponentialDecay(initial_learning_rate=INITIAL_LEARNING_RATE,
                                decay_steps=DECAY_STEPS, decay_rate=DECAY_RATE, staircase=False, name=None)
 optimizer = Adam(lr_schedule)
